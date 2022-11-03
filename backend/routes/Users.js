0c3605971bf745ed99afd2c94a86b30956606ee2
M Waseem <wasirana80@gmail.com>
2022-11-03 17:27:58 +0500

    Update Users.js

--- backend/routes/Users.js
+++ backend/routes/Users.js
@@ -1,86 +1,82 @@
 const express = require("express");
 const Users = require("../models/Users");
-
+const { v4: uuid } = require("uuid");
 const router = express.Router();
 
-
-
 router.post("/postUser", async (req, res) => {
-
-    try {
-        const value = req.body
-        if (!value) res.json({ success: false, message: "Invalid Data" })
-        console.log(value)
-        const response = await Users.create(value)
-        if (response)
-            res.json({ success: true, result: response });
-        else
-            res.json({ success: false, message: "User Not created" });
-    } catch (err) {
-        res.status(500).json({ success: false, message: err });
-
-    }
-})
+  try {
+    const value = req.body;
+    if (!value) res.json({ success: false, message: "Invalid Data" });
+    console.log(value);
+    const response = await Users.create(value);
+    if (response) res.json({ success: true, result: response });
+    else res.json({ success: false, message: "User Not created" });
+  } catch (err) {
+    res.status(500).json({ success: false, message: err });
+  }
+});
 
 router.get("/getUsers", async (req, res) => {
+  try {
+    let data = await Users.find({});
 
-
-    try {
-
-        let data = await Users.find({})
-
-
-
-
-
-        if (data.length)
-            res.json({ success: true, result: data });
-        else
-            res.json({ success: false, message: "User Not found" });
-    } catch (err) {
-        res.status(500).json({ success: false, message: err });
-
-    }
-})
+    if (data.length) res.json({ success: true, result: data });
+    else res.json({ success: false, message: "User Not found" });
+  } catch (err) {
+    res.status(500).json({ success: false, message: err });
+  }
+});
 router.post("/getUser", async (req, res) => {
-
-console.log(req.body)
-    try {
-        let { user_uuid } = req.body
-        let data = await Users.findOne({$or:[{user_name:user_uuid},{user_uuid}] })
-        // console.log(data, user_name)
-        if (data)
-            res.json({ success: true, result: data });
-        else
-            res.json({ success: false, message: "User Not found" });
-    } catch (err) {
-        res.status(500).json({ success: false, message: err });
-
+  console.log(req.body);
+  try {
+    let { user_uuid } = req.body;
+    let data = await Users.findOne({
+      $or: [{ user_name: user_uuid }, { user_uuid }],
+    });
+    // console.log(data, user_name)
+    if (data) res.json({ success: true, result: data });
+    else res.json({ success: false, message: "User Not found" });
+  } catch (err) {
+    res.status(500).json({ success: false, message: err });
+  }
+});
+router.post("/varifyUser", async (req, res) => {
+  try {
+    let { user_name } = req.body;
+    let data = await Users.findOne({ user_name });
+    console.log(data, user_name)
+    if (data) res.json({ success: true, result: data });
+    else {
+        let value={ user_name, user_uuid: uuid() }
+        console.log(value)
+      let response = await Users.create(value);
+      if (response) res.json({ success: true, result: value });
+      else res.json({ success: false, message: "User Not found" });
     }
-})
+  } catch (err) {
+    res.status(500).json({ success: false, message: err });
+  }
+});
 
 router.put("/putUser", async (req, res) => {
-    try {
-
-        let value = req.body
-        if (!value) res.json({ success: false, message: "Invalid Data" })
-        value = Object.keys(value)
-            .filter((key) => key !== "_id")
-            .reduce((obj, key) => {
-                obj[key] = value[key];
-                return obj;
-            }, {});
-        const response = await Users.updateOne({ user_uuid: value.user_uuid }, value)
-        if (response.acknowledged)
-            res.json({ success: true, result: value });
-        else
-            res.json({ success: false, message: "user Not updated" });
-    } catch (err) {
-        res.status(500).json({ success: false, message: err });
-
-    }
-})
-
-
+  try {
+    let value = req.body;
+    if (!value) res.json({ success: false, message: "Invalid Data" });
+    value = Object.keys(value)
+      .filter((key) => key !== "_id")
+      .reduce((obj, key) => {
+        obj[key] = value[key];
+        return obj;
+      }, {});
+    const response = await Users.updateOne(
+      { user_uuid: value.user_uuid },
+      value
+    );
+    if (response.acknowledged) res.json({ success: true, result: value });
+    else res.json({ success: false, message: "user Not updated" });
+  } catch (err) {
+    res.status(500).json({ success: false, message: err });
+  }
+});
 
 module.exports = router;
