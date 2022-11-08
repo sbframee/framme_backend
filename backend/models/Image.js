156c62f8fbffd22e38a892da7fa1e9ec45cb2159
M Waseem <wasirana80@gmail.com>
2022-11-08 17:28:17 +0500

    Update Image.js

--- backend/models/Image.js
+++ backend/models/Image.js
@@ -1,50 +1,54 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const imageSchema = new mongoose.Schema({
-    img_url: {
-        type: String,
-
+  img_url: {
+    type: String,
+  },
+  thumbnail_url: {
+    type: String,
+  },
+  img_type: {
+    type: String,
+  },
+  user_category_uuid: [{ type: String }],
+  user_sub_category_uuid: [{ type: String }],
+  occ_uuid: [
+    {
+      occ_uuid: { type: String },
+      sort_order: { type: String },
     },
-    thumbnail_url: {
-        type: String,
+  ],
+  sort_order: { type: String },
+  coordinates: [
+    {
+      base_img_url: { type: String },
+      a: { type: String },
+      b: { type: String },
+      c: { type: String },
+      d: { type: String },
     },
-    img_type: {
+  ],
+  user: [{ type: String }],
+  holder: [
+    {
+      text_color: {
         type: String,
-
+      },
+      fontFamily: {
+        type: String,
+      },
+      label_uuid: { type: String },
+      a: { type: String },
+      b: { type: String },
+      c: { type: String },
+      d: { type: String },
     },
-    user_category_uuid: [{ type: String }],
-    user_sub_category_uuid: [{ type: String }],
-    occ_uuid: [{
-        occ_uuid: { type: String }, sort_order: { type: String }
-    }],
-    sort_order: { type: String },
-    coordinates: [{
-        base_img_url: { type: String },
-        a: { type: String },
-        b: { type: String },
-        c: { type: String },
-        d: { type: String },
-    }],
-    user: [{ type: String }],
-    holder: [{
-        text_color: {
-            type: String,
+  ],
+  cat_uuid: { type: String },
+  tag_uuid: { type: String },
+  text: { type: String },
+  fontFamily: { type: String },
+  text_color: { type: String },
+});
 
-        },
-        fontFamily: {
-            type: String,
-
-        },
-        label_uuid: { type: String },
-        a: { type: String },
-        b: { type: String },
-        c: { type: String },
-        d: { type: String },
-    }],
-    cat_uuid: { type: String },
-    tag_uuid: { type: String },
-    text: { type: String },
-})
-
-
-module.exports = mongoose.model('images', imageSchema)
+module.exports = mongoose.model("images", imageSchema);
