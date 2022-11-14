801576131238e5f66126446ab127c93ebf08e6e6
M Waseem <wasirana80@gmail.com>
2022-11-14 14:40:49 +0500

    Update Tags.js

--- backend/models/Tags.js
+++ backend/models/Tags.js
@@ -1,32 +1,33 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const TagsSchema = new mongoose.Schema({
-    tag_title: {
-        type: String,
-    },
-    icon_url: {
-        type: String,
+  tag_title: {
+    type: String,
+  },
+  icon_url: {
+    type: String,
+  },
+  tag_uuid: {
+    type: String,
+  },
+  tag_type: {
+    type: String,
+  },
+  min: {
+    type: String,
+  },
+  max: {
+    type: String,
+  },
+  height: {
+    type: String,
+  },
+  width: {
+    type: String,
+  },
+  scale: {
+    type: String,
+  },
+});
 
-    },
-    tag_uuid: {
-        type: String,
-
-    },
-    tag_type: {
-        type: String,
-
-    },
-    min: {
-        type: String,
-
-    },
-    max: {
-        type: String,
-
-    },
-
-
-})
-
-
-module.exports = mongoose.model('tags', TagsSchema)
+module.exports = mongoose.model("tags", TagsSchema);
